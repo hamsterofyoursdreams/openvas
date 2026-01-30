076b7357f171d2d0948f041a33da5d7c30887cba
hamsterofyoursdreams <130088894+hamsterofyoursdreams@users.noreply.github.com>
2026-01-31 01:00:20 +0300

    Update scan-inst.sh

--- scan-inst.sh
+++ scan-inst.sh
@@ -165,11 +165,14 @@ install_common_dep() {
   log INFO "Installing common build dependencies..."
     
   # Очистка кэша
-  run_command apt clean
+  run_command apt purge systemd-timesyncd
+  run_command dpkg --purge systemd-timesyncd
+  run_command apt autoremove
+  run_command apt autoclean
   run_command apt update
     
   if ! run_command apt install --no-install-recommends --assume-yes \
-    build-essential curl cmake pkg-config python3 python3-pip gnupg snmp libsnmp-base; then
+    build-essential curl cmake pkg-config python3 python3-pip gnupg libsnmp-dev; then
     log ERROR "Failed to install common dependencies. Check apt configuration."
     exit 1
   fi
