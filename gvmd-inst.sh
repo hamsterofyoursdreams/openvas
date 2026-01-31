aea5a315362e983109084b3459e0e4038abb4b1f
hamsterofyoursdreams <130088894+hamsterofyoursdreams@users.noreply.github.com>
2026-02-01 00:21:55 +0300

    Update gvmd-inst.sh

--- gvmd-inst.sh
+++ gvmd-inst.sh
@@ -1165,11 +1165,11 @@ trap cleanup EXIT
 main() {
   log INFO "Starting GVMD machine  installation on $(date '+%Y-%m-%d %H:%M:%S')..."
 
-  # Устанавливаем необходимые пакеты для OpenVAS
-  install_packages
-
   # Устанавливаем переменные окружения для установки
   set_environment
+  
+  # Устанавливаем необходимые пакеты для OpenVAS
+  install_packages
 
   # Проверяем последние версии компонентов
   check_latest_version
